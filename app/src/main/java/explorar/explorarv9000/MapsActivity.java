b6e46def2ee9a7dc6532bca5efd1225b2a9e56ac
michaelliangaustralia <michaelliang15@gmail.com>
2017-09-23 19:22:12 +1000

    All MVP base level functionality done.

--- app/src/main/java/explorar/explorarv9000/MapsActivity.java
+++ app/src/main/java/explorar/explorarv9000/MapsActivity.java
@@ -1,16 +1,12 @@
 package explorar.explorarv9000;
 
 import android.Manifest;
-import android.app.IntentService;
-import android.content.Intent;
+
 import android.content.IntentSender;
-import android.content.pm.LauncherApps;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
+
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -20,35 +16,20 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.FusedLocationProviderApi;
-import com.google.android.gms.location.FusedLocationProviderClient;
 
-import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import com.google.android.gms.location.LocationRequest;
 
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.location.SettingsClient;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
@@ -57,12 +38,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     private LocationRequest mLocationRequest;
 
-
     private static final int DEFAULT_ZOOM = 15;
 
 
 //    private FusedLocationProviderClient mFusedLocationClient;
-//    private Location mCurrentLocation;
+    private Location mCurrentlocation;
 //    private LocationCallback mLocationCallback;
 
     private GoogleMap mMap;
@@ -79,50 +59,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-//
-//        //Get Current Location Settings - https://developer.android.com/training/location/change-location-settings.html#prompt
-//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-//                .addLocationRequest(mLocationRequest);
-//
-//        SettingsClient client = LocationServices.getSettingsClient(this);
-//        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-//
-//        //Prompt the User to Change Location Settings - https://developer.android.com/training/location/change-location-settings.html#prompt
-//        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
-//            @Override
-//            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-//                // All location settings are satisfied. The client can initialize location requests here.
-//                // ...
-//            }
-//        });
-//
-//        task.addOnFailureListener(this, new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                int statusCode = ((ApiException) e).getStatusCode();
-//                switch (statusCode) {
-//                    case CommonStatusCodes.RESOLUTION_REQUIRED:
-//                        // Location settings are not satisfied, but this can be fixed
-//                        // by showing the user a dialog.
-//                        try {
-//                            // Show the dialog by calling startResolutionForResult(),
-//                            // and check the result in onActivityResult().
-//                            ResolvableApiException resolvable = (ResolvableApiException) e;
-//                            resolvable.startResolutionForResult(MapsActivity.this,
-//                                    REQUEST_CHECK_SETTINGS);
-//                        } catch (IntentSender.SendIntentException sendEx) {
-//                            // Ignore the error.
-//                        }
-//                        break;
-//                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-//                        // Location settings are not satisfied. However, we have no way
-//                        // to fix the settings so we won't show the dialog.
-//                        break;
-//                }
-//            }
-//        });
-
 
 
         // Create the LocationRequest object
@@ -181,119 +117,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //set mMap to GoogleMap view
         mMap = googleMap;
 
-        //set default camera to UNSW latlong
-//        LatLng unsw = new LatLng(-33.919728, 151.234095);
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(unsw));
-//        mMap.moveCamera(CameraUpdateFactory.zoomTo(14.0f)); //TODO: Make the zoom look pretty
-//        //TODO: Would be nice to twist the orientation as well so that you look up main walkway
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-33.919728, 151.234095);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in UNSW"));
+        //set default camera to UNSWlatlng and move the camera
+        LatLng unswLatLng = new LatLng(-33.917378, 151.230205);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(unswLatLng));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(14.0f)); //TODO: Make the zoom look pretty
+        //TODO: Would be nice to twist the orientation as well so that you look up main walkway
+
+        // Add a marker in Sydney
+        LatLng sydney = new LatLng(-33.919728, 151.234095);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("This is an example event"));
 
         //Checking for location permissions and enabling current location or requesting necessary permissions
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_ACCESS_FINE_LOCATION);
 
-
         //Remove the location icon on the top right
-        //mMap.getUiSettings().setMyLocationButtonEnabled(false); //TODO : Once finished, Remove the location button in the top right
+        //mMap.getUiSettings().setMyLocationButtonEnabled(false); //TODO : Once finished and if location is automatic, Remove the location button in the top right
 
         //Setting Toast to appear on location button click
         mMap.setOnMyLocationButtonClickListener(this);
-//
-//        //Create location service client
-//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-//
-//        //Getting last known Location and storing it in a Location object
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            //Permission check failed
-//            Toast permissionsToast = new Toast(this);
-//            permissionsToast = Toast.makeText(this, "No permissions", Toast.LENGTH_LONG);
-//            permissionsToast.show();
-//            return;
-//        } else {
-//            mFusedLocationClient.getLastLocation()
-//                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-//
-//                        @Override
-//                        public void onSuccess(Location location) {
-//                            //Got last known location. In some rare situations this can be null
-//                            if (location != null) {
-//                                //Logic to handle location object - generate toast
-//                                Toast.makeText(getBaseContext(), "the last location is" + location, Toast.LENGTH_LONG).show();
-//
-//                                //Set last known location to current location
-//                                mCurrentLocation = location;
-//
-//                            }
-//                        }
-//                    });
-//        }
-//
-//
-//        //TODO: FIX THIS
-//        mLocationCallback = new LocationCallback() {
-//
-//            @Override
-//            public void onLocationResult(LocationResult locationResult) {
-//                for (Location location : locationResult.getLocations()){
-//                    double latitude = location.getLatitude();
-//                    double longitude = location.getLongitude();
-//                    LatLng latLng = new LatLng(latitude,longitude);
-//
-//                    Toast.makeText(getBaseContext(),"Update the UI with " + latLng, Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        };
-
-
-        //TODO: FIX THIS
-        //Request Location Updates - https://developer.android.com/training/location/receive-location-updates.html
-
-//        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
 
     }
-
-//    private void startLocationUpdates() {
-//
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            return;
-//        }
-//        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
-//    }
-
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        boolean mRequestingLocationUpdates = true; //TODO: this has been hardcoded - change this - https://developer.android.com/training/location/receive-location-updates.html
-//        if (mRequestingLocationUpdates) {
-//            startLocationUpdates();
-//        }
-//    }
-
-
-//    @Override  //To define mRequestingLocationUpdates ("is the system requesting location updates") - followed https://developer.android.com/training/location/receive-location-updates.html#save-state
-//    protected void onSaveInstanceState(Bundle outState) {
-//        outState.putBoolean(REQUESTING_LOCATIONS_UPDATES_KEY,mRequestingLocationUpdates);
-//        super.onSaveInstanceState(outState);
-//    }
-//
-//    private void updateValuesFromBundle(Bundle savedInstanceState) {
-//        //Update the value of mRequestingLocationUpdates from the Bundle
-//        if (savedInstanceState.keySet().contains(REQUESTING_LOCATIONS_UPDATES_KEY)){
-//            mRequestingLocationUpdates = savedInstanceState.getBoolean(REQUESTING_LOCATIONS_UPDATES_KEY);
-//        }
-//
-//        //TODO: There is an "UpdateUI()" method called in example. Not sure if I need to do this? https://developer.android.com/training/location/receive-location-updates.html#save-state
-//    }
-//
-
 
     @Override  //Defines what happens when you click the location button
     public boolean onMyLocationButtonClick() {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_LONG).show();
         Log.d(TAG, "onMyLocationButtonClick Button Clicked");
+
+        if (mGoogleApiClient != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+            else{
+                Log.i(TAG, "LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this); evoked");
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            }
+        }
         return false;
+
     }
 
     @Override
@@ -303,12 +163,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             return;
         } else {
-            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (location == null) {
+
+            mCurrentlocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//            if (location == null) {
                 LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-            } else {
-                handleNewLocation(location);
-            }
+//            } else {
+//                handleNewLocation(location);
+//            }
 
         }
     }
@@ -326,7 +187,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onConnectionSuspended(int i) {
         Log.i(TAG, "Location services suspended. Please reconnect.");
-
     }
 
     @Override
@@ -352,6 +212,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private void setUpMapIfNeeded(){ //Maybe useless
     Log.i (TAG, "setUpMapIfNeeded evoked");
+
+
     }
 
     @Override
@@ -368,16 +230,93 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         handleNewLocation(location);
     }
 
-//    //Set up location request - https://developer.android.com/training/location/change-location-settings.html#prompt
-//    protected void createLocationRequest() {
-//        LocationRequest mLocationRequest = new LocationRequest();
-//        mLocationRequest.setInterval(1000); //interval between location updates in milliseconds
-//        mLocationRequest.setFastestInterval(5000); //fastest interval = xxx milliseconds per request
-//        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-//    }
-
 }
 
 
 
-//Make the user's location appear upon opening of the app
+//TODO: Make the user's location appear upon opening of the app
+
+
+/*
+        //Place current location marker
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+
+//        //Create location service client
+//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+////
+//        //Getting last known Location and storing it in a Location object
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            //Permission check failed
+//            Toast permissionsToast = new Toast(this);
+//            permissionsToast = Toast.makeText(this, "No permissions", Toast.LENGTH_LONG);
+//            permissionsToast.show();
+//            return;
+//        } else {
+//            mFusedLocationClient.getLastLocation()
+//                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//
+//                        @Override
+//                        public void onSuccess(Location location) {
+//                            //Got last known location. In some rare situations this can be null
+//                            if (location != null) {
+//                                //Logic to handle location object - generate toast
+//                                Toast.makeText(getBaseContext(), "the last location is" + location, Toast.LENGTH_LONG).show();
+//
+//                                //Set last known location to current location
+//                                mLastKnownLocation = location;
+//
+//                            }
+//                        }
+//                    });
+//        }
+//
+
+--in oncreate--
+        //Get Current Location Settings - https://developer.android.com/training/location/change-location-settings.html#prompt
+//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+//                .addLocationRequest(mLocationRequest);
+//
+//        SettingsClient client = LocationServices.getSettingsClient(this);
+//        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+//
+//        //Prompt the User to Change Location Settings - https://developer.android.com/training/location/change-location-settings.html#prompt
+//        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+//            @Override
+//            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+//                // All location settings are satisfied. The client can initialize location requests here.
+//                // ...
+//            }
+//        });
+//
+//        task.addOnFailureListener(this, new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                int statusCode = ((ApiException) e).getStatusCode();
+//                switch (statusCode) {
+//                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+//                        // Location settings are not satisfied, but this can be fixed
+//                        // by showing the user a dialog.
+//                        try {
+//                            // Show the dialog by calling startResolutionForResult(),
+//                            // and check the result in onActivityResult().
+//                            ResolvableApiException resolvable = (ResolvableApiException) e;
+//                            resolvable.startResolutionForResult(MapsActivity.this,
+//                                    REQUEST_CHECK_SETTINGS);
+//                        } catch (IntentSender.SendIntentException sendEx) {
+//                            // Ignore the error.
+//                        }
+//                        break;
+//                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+//                        // Location settings are not satisfied. However, we have no way
+//                        // to fix the settings so we won't show the dialog.
+//                        break;
+//                }
+//            }
+//        });
+ */
