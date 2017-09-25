2e13c94eb777049f66d3de8531a1364c3003e665
michaelliangaustralia <michaelliang15@gmail.com>
2017-09-25 13:31:18 +1000

    IT FUCKING WORKS YEAH THE BOYS
    Intent to open up contentview event detail is done

---
+++ app/src/main/java/explorar/explorarv9000/EventDetailsActivity.java
@@ -0,0 +1,20 @@
+package explorar.explorarv9000;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michaelliang on 25/9/17.
+ */
+
+public class EventDetailsActivity extends AppCompatActivity{
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_detail);
+
+    }
+}
--- app/src/main/java/explorar/explorarv9000/MainActivity.java
+++ app/src/main/java/explorar/explorarv9000/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"shit fucked", Toast.LENGTH_LONG);
+        Toast.makeText(this,"shit fucked", Toast.LENGTH_LONG).show();
     }
 }
 
